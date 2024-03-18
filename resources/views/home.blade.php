ef9cdfeb9e7321079d5a343077a2a24e10f53e98
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-03-19 05:48:59 +0700

    Add UI Home Page Navbar 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,10 +10,10 @@
 
     </head>
     <body>
-        <nav class="navbar navbar-dark navbar-expand-lg bg-primary">
+        <nav class="navbar navbar-dark navbar-expand-lg bg-secondary">
             <div class="container flex justify-content-between">
               <a class="navbar-link" href="#">
-                <img src="">
+                <img class="h-32px" src="{{ url('assets/images/forum-logos.png') }}" alt="ihbsforum logo">
               </a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
