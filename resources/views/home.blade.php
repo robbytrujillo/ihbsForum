131b78b8008860e58166734dbaac4e2a9a8feeac
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-03-24 08:38:58 +0700

    Add UI Home Page | Footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -188,6 +188,45 @@
           </div>
         </section>
 
+        {{-- Footer Section --}}
+        <footer class="py-80px">
+          <div class="container">
+            <div class="row">
+              <div class="col-12 col-lg-6 mb-5 mb-lg-0">
+                <img src="{{ url('assets/images/ihbsforum-logos.png') }}" alt="ihbs logo" class="h-28px mb-4">
+                <p>Empowering The IHBS Community <br> to connect, share, and learn</p>
+            </div>
+            <div class="col-12 col-lg-6 me-auto">
+              <div class="d-flex flex-column flex-lg-row justify-content-end">
+                <div class="d-flex flex-column me-140px mb-3 mb-lg-0">
+                  <p class="fw-bold fs-5 text-green text-nowrap">
+                    Contact Us
+                  </p>
+                  <ul class="list-unstyled">
+                    <li class="text-black">
+                      <a href="mailto:robbyikusuma88@gmail.com">robbyikusuma88@gmail.com</a>
+                    </li>
+                  </ul>
+                </div>
+                <div class="d-flex flex-column">
+                  <p class="fw-bold fs-5 text-green">Links</p>
+                  <ul class="list-unstyled">
+                    <li class="text-black mb-2">
+                      <a href="#">Home</a>
+                    </li>
+                    <li class="text-black mb-2">
+                      <a href="#">Discussions</a>
+                    </li>
+                    <li class="text-black mb-2">
+                      <a href="#">About Us</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     </body>
 </html>
