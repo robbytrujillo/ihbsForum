b4d1b383cf16c752103dd1dbcbdf58da825bc718
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-03-27 05:32:49 +0700

    Add UI Login Page

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -4,7 +4,11 @@
     <section class="bg-gray vh-100">
         <div class="container h-100 pt-5">
             <div class="row justify-content-center">
-                
+                <div class="col-12 col-lg-3">
+                    <a href="" class="nav-link mb-5 text-center">
+
+                    </a>
+                </div>
             </div>
         </div>
     </section>
