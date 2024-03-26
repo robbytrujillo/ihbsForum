f2b53a6ce9f4d6f45cc184edf7062bd5aa43f2ad
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-03-26 19:16:17 +0700

    Add UI Login Page

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.auth')
 
 @section('body')
-
+    <section class="bg-gray vh-100">
+        <div class="container h-100 pt-5">
+            <div class="row justify-content-center">
+                
+            </div>
+        </div>
+    </section>
 @endsection
