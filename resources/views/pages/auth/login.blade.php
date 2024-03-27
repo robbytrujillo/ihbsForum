de52313b2af9bc887a9ab8c6b1c43a57219290a4
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-03-28 06:22:02 +0700

    Add UI Login Page

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -11,8 +11,12 @@
                     <div class="card mb-5">
                         <form action="">
                             <div class="mb-3">
-                                <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                                <label for="email" class="form-label">Email</label>
+                                <input type="email" class="form-control" id="email" placeholder="name@example.com" autocomplete="off" autofocus>
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="password">
                             </div>
                         </form>
                     </div>
